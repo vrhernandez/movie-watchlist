a50ca9f1cedcde9160fed1eb4726d1e7211855b0
vrhernandez <vicenterh99@gmail.com>
2025-05-05 22:28:30 -0700

    adding input functionality to search bar, working on displaying results on page

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-let title = "blade runner"
+const searchForm = document.getElementById("search-form")
+const searchResultsEl = document.getElementById("search-results")
 
-fetch(`http://www.omdbapi.com/?apikey=a9731a9b&s=${title}`)
-    .then(res => res.json())
-    .then(data => {
+searchForm.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const title = document.getElementById("title").value
+    //console.log("Searching for: " + title)
 
+    searchResultsEl.innerHTML = getSearchResults(title)
+
+    // fetch(`http://www.omdbapi.com/?apikey=a9731a9b&t=${title}`)
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         //console.log("Response: " + data.Response)
+    //         let resultHTML = ""
+
+    //         if(data.Response==="True"){
+    //             //console.log("Response was true!")
+    //             let results = data.Search
+    //             for(let result of results){
+    //                 resultHTML += `<p>${result.Title}</p>`
+    //             }
+    //         } else{resultHTML = "Error: " + data.Error}
+
+    //         searchResultsEl.innerHTML = resultHTML
+    //     })
+})
+
+async function getSearchResults(title) {
+    const res = await fetch(`http://www.omdbapi.com/?apikey=a9731a9b&t=${title}`)
+    const data = await res.json()
+    
+    console.log(data)
+    //console.log("Response: " + data.Response)
+    let resultHTML = ""
+
+    if(data.Response==="True"){
+        //console.log("Response was true!")
         let results = data.Search
         for(let result of results){
-        console.log(result.Title)
+            resultHTML += `<p>${result.Title}</p>`
         }
-    })
+    } else{resultHTML = "Error: " + data.Error}
+
+    return resultHTML
+}
+
