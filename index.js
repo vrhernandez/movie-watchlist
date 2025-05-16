12763f649012367f25fec10ccacf4f66c8b01cc8
vrhernandez <vicenterh99@gmail.com>
2025-05-15 21:27:10 -0700

    Continued building
    added some images for backgrounds and testing, beginning styling

--- index.js
+++ index.js
@@ -6,43 +6,28 @@ searchForm.addEventListener("submit", (e)=>{
     const title = document.getElementById("title").value
     //console.log("Searching for: " + title)
 
-    searchResultsEl.innerHTML = getSearchResults(title)
+    getSearchResults(title)
 
-    // fetch(`http://www.omdbapi.com/?apikey=a9731a9b&t=${title}`)
-    //     .then(res => res.json())
-    //     .then(data => {
-    //         console.log(data)
-    //         //console.log("Response: " + data.Response)
-    //         let resultHTML = ""
-
-    //         if(data.Response==="True"){
-    //             //console.log("Response was true!")
-    //             let results = data.Search
-    //             for(let result of results){
-    //                 resultHTML += `<p>${result.Title}</p>`
-    //             }
-    //         } else{resultHTML = "Error: " + data.Error}
-
-    //         searchResultsEl.innerHTML = resultHTML
-    //     })
 })
 
 async function getSearchResults(title) {
-    const res = await fetch(`http://www.omdbapi.com/?apikey=a9731a9b&t=${title}`)
+    const res = await fetch(`http://www.omdbapi.com/?apikey=a9731a9b&s=${title}`)
     const data = await res.json()
     
-    console.log(data)
+    //console.log(data)
     //console.log("Response: " + data.Response)
     let resultHTML = ""
 
     if(data.Response==="True"){
         //console.log("Response was true!")
         let results = data.Search
+        //console.log("results is: "+results)
         for(let result of results){
             resultHTML += `<p>${result.Title}</p>`
         }
     } else{resultHTML = "Error: " + data.Error}
 
-    return resultHTML
+    //console.log("resultHTML is: "+resultHTML)
+    searchResultsEl.innerHTML = resultHTML
 }
 
